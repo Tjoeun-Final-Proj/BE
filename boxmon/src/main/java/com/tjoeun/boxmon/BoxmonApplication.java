4930fb2de509b0148f1e576a02cf2e61f2ff0a7f
Naseoi1022 <nsi1022@naver.com>
2026-02-05 12:34:19 +0900

    기본구조설정

---
+++ boxmon/src/main/java/com/tjoeun/boxmon/BoxmonApplication.java
@@ -0,0 +1,13 @@
+package com.tjoeun.boxmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxmonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxmonApplication.class, args);
+    }
+
+}
---
+++ boxmon/src/test/java/com/tjoeun/boxmon/BoxmonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjoeun.boxmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxmonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
