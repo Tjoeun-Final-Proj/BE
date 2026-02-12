2695e4ea7ec2ce182079648f5a2cbd51eb02af72
kanghyeongwon5672 <150598329+kanghyeongwon5672@users.noreply.github.com>
2026-02-12 13:42:43 +0900

    feat : 화주 내 화물 목록 조회 기능 구현

--- boxmon/src/main/java/com/tjoeun/boxmon/feature/shipment/repository/ShipmentRepository.java
+++ boxmon/src/main/java/com/tjoeun/boxmon/feature/shipment/repository/ShipmentRepository.java
@@ -1,9 +1,18 @@
 package com.tjoeun.boxmon.feature.shipment.repository;
 
 import com.tjoeun.boxmon.feature.shipment.domain.Shipment;
+import com.tjoeun.boxmon.feature.shipment.domain.ShipmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+
+    // 1. 특정 화주의 전체 화물을 최신 등록순으로 조회
+    List<Shipment> findByShipper_ShipperIdOrderByCreatedAtDesc(Long shipperId);
+
+    // 2. 특정 화주의 화물을 상태별 필터링하여 최신 등록순으로 조회
+    List<Shipment> findByShipper_ShipperIdAndShipmentStatusOrderByCreatedAtDesc(Long shipperId, ShipmentStatus shipmentStatus);
 }
