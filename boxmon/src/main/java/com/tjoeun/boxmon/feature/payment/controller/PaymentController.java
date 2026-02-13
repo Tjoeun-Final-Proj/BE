94b054bf39f418f822f8f1dfc1a3a77bf1ecf668
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2026-02-13 11:02:07 +0900

    fix: 결제 승인 완료시 메소드 미구현 오류를 발생시키던 부분 수정

--- boxmon/src/main/java/com/tjoeun/boxmon/feature/payment/controller/PaymentController.java
+++ boxmon/src/main/java/com/tjoeun/boxmon/feature/payment/controller/PaymentController.java
@@ -5,14 +5,11 @@ import com.tjoeun.boxmon.feature.payment.service.PaymentService;
 import com.tjoeun.boxmon.feature.shipment.repository.ShipmentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -26,6 +23,6 @@ public class PaymentController {
     public ResponseEntity<String> confirmPayment(@RequestBody ConfirmPaymentRequest request) {
         log.info("request={}",request);
         paymentService.confirmPayment(request);
-        throw new NotImplementedException("not yet implemented"); //TODO 서비스 구현이 끝나면 삭제해야함
+        return ResponseEntity.ok("결제가 승인되었습니다.");
     }
 }
