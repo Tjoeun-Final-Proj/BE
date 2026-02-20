240c8fd0e6bef32e6f358e8d59472fee6abe19c4
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2026-02-20 17:39:37 +0900

    feat: 유스케이스별 알림 메소드 구현

--- boxmon/src/test/java/com/tjoeun/boxmon/NotificationTests.java
+++ boxmon/src/test/java/com/tjoeun/boxmon/NotificationTests.java
@@ -2,6 +2,7 @@ package com.tjoeun.boxmon;
 
 import com.tjoeun.boxmon.feature.notification.domain.NotificationType;
 import com.tjoeun.boxmon.feature.notification.service.NotificationSender;
+import com.tjoeun.boxmon.feature.notification.service.NotificationUseCase;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,17 +19,25 @@ import java.util.Map;
 public class NotificationTests {
     private static final Logger log = LoggerFactory.getLogger(NotificationTests.class);
     @Autowired
-    NotificationSender notificationService;
+    NotificationSender notificationSender;
     
     @Test
     public void testNotification() {
-        notificationService.send(
+        notificationSender.send(
                 1L, 
                 1L,
                 NotificationType.ASSIGNMENT_CANCELLATION_REQUESTED,
                 "테스트 메시지 제목",
-                "테스트 메시지 내용aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                "2/20 테스트",
                 Map.of("content","test payload")
         );
+    }
+    
+    @Autowired
+    NotificationUseCase notificationUseCase;
+    
+    @Test
+    public void testNotification2() {
+        notificationUseCase.notifyAssignmentCompleted(1);
     }
 }
