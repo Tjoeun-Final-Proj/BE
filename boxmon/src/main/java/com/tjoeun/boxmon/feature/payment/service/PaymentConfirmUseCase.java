694a58337947fc463e6bcce0b972fbeca28f9440
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2026-02-24 10:36:46 +0900

    rename: 역할에 맞게 이름 변경 PaymentUseCase -> PaymentConfirmUseCase

--- boxmon/src/main/java/com/tjoeun/boxmon/feature/payment/service/PaymentConfirmUseCase.java
+++ boxmon/src/main/java/com/tjoeun/boxmon/feature/payment/service/PaymentConfirmUseCase.java
@@ -2,6 +2,6 @@ package com.tjoeun.boxmon.feature.payment.service;
 
 import com.tjoeun.boxmon.feature.payment.dto.ConfirmPaymentRequest;
 
-public interface PaymentUseCase {
+public interface PaymentConfirmUseCase {
     void confirmPayment(ConfirmPaymentRequest request);
 }
