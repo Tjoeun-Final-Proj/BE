3a6057c6491891fde157080ed1a9030d8a37610f
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2026-02-26 18:31:00 +0900

    feat: 셀러 등록 api 호출 구현

--- boxmon/src/main/java/com/tjoeun/boxmon/feature/payment/client/TossApiClient.java
+++ boxmon/src/main/java/com/tjoeun/boxmon/feature/payment/client/TossApiClient.java
@@ -33,7 +33,6 @@ public class TossApiClient {
                 "orderId", orderId,
                 "amount", amount
         );
-        
 
         ResponseEntity<Map<String,Object>> result = client.post()
                 .uri("/v1/payments/confirm")
@@ -86,6 +85,49 @@ public class TossApiClient {
                 });
 
         log.debug("결제 취소 성공. 응답: result={}",result);
+
+        if (result.getStatusCode().value()>=400)
+            throw new ExternalServiceException("토스 서버 통신 실패. 응답: " + result.getBody());
+    }
+    
+    //차주의 셀러등록 요청
+    public void registerDriver(String driverId, String name, String email, String phone, String bankCode, String accountNumber, String accountHolderName){
+        log.info("차주의 셀러등록을 요청합니다...");
+        Map<String, Object> requestBody = Map.of(
+                "refSellerId", driverId,
+                "businessType", "INDIVIDUAL",
+                "individual", Map.of(
+                        "name", name,
+                        "email", email,
+                        "phone", phone
+                ),
+                "account", Map.of(
+                        "bankCode", bankCode,
+                        "accountNumber", accountNumber,
+                        "holderName", accountHolderName
+                )
+        );
+
+        ResponseEntity<Map<String,Object>> result = client.post()
+                .uri("/v2/sellers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(requestBody)
+                .exchange((req, resp) -> {
+                    int raw = resp.getStatusCode().value();
+
+                    // 성공/실패 상관없이 Map으로 읽기 시도
+                    Map<String, Object> body = null;
+                    try {
+                        body = resp.bodyTo(Map.class);
+                    } catch (Exception ignore) {
+                        // 바디가 JSON이 아니거나 비어있으면 null 유지
+                    }
+
+                    return ResponseEntity.status(raw).headers(resp.getHeaders()).body(body);
+                });
+
+        log.debug("셀러 등록 성공. 응답: result={}",result);
 
         if (result.getStatusCode().value()>=400)
             throw new ExternalServiceException("토스 서버 통신 실패. 응답: " + result.getBody());
