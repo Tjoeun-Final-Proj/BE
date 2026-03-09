4e64b8975d8da030f1f45741d7d5c8ed0a711e7a
Naseoi1022 <nsi1022@naver.com>
2026-03-09 18:02:21 +0900

    [fix/#72] seller넣어서 계좌 입력

--- boxmon/src/main/java/com/tjoeun/boxmon/feature/user/repository/DriverRepository.java
+++ boxmon/src/main/java/com/tjoeun/boxmon/feature/user/repository/DriverRepository.java
@@ -1,13 +1,10 @@
 package com.tjoeun.boxmon.feature.user.repository;
 
 import com.tjoeun.boxmon.feature.user.domain.Driver;
-import com.tjoeun.boxmon.feature.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface DriverRepository extends JpaRepository<Driver, Long> {
-    Optional<Driver> findByUser(User user);
-
-    Driver findByUser_UserId(Long userId);
+    Optional<Driver> findByUser_UserId(Long userId);
 }
